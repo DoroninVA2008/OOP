5d4edce0e305d446c00259aeab81af4fb1d5229a
DoroninVA2008 <hotlive95334@gmail.com>
2026-07-02 12:05:04 +0500

    ComPush after creating new branch by 9th Task for 9th week in OOP/week-09 10

--- 8th_task/src/1 theor/3 useEffect.tsx
+++
@@ -1,9 +0,0 @@
-useEffect(() => {
-  const fetchData = async () => {
-    const response = await fetch('/api/data');
-    // Указываем тип ожидаемых данных
-    const data: MyDataType = await response.json();
-    setState(data);
-  };
-  fetchData();
-}, []);
