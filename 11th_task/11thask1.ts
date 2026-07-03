bb6932593688b03e3a08693b4152687934c9ede4
DoroninVA2008 <hotlive95334@gmail.com>
2026-07-03 11:54:46 +0500

    ComPush after creating new branch by 12th Task for 12th week in OOP/week-12 14

--- 11th_task/11thask1.ts
+++
@@ -1,3 +0,0 @@
-import { User } from '../1 Utility_Types'
-
-type UserEditForm = Partial<User> & { id: User['id'] };
--- 11th_task/11thask2.js
+++
@@ -1,6 +0,0 @@
-function processData(input) {
-    return {};
-}
-function isProcessSuccess(arg) {
-    return typeof arg === 'object' && arg !== null && 'status' in arg && arg.status === 'success';
-}
